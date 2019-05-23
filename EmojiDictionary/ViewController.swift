bb257ec5418b04f0a79471937e51dbb91e2c9e64
Jolyon Rudolph <jolyonrudolph@gmail.com>
2019-05-23 13:34:53 +0200

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Jolyon Rudolph on 23/05/2019.
+//  Copyright © 2019 Jolyon Rudolph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
