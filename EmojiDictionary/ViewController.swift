efc957f75655fd2671743d7b3651ad366c144f88
Jolyon Rudolph <jolyonrudolph@gmail.com>
2019-05-23 13:43:39 +0200

    first commit

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var emojiTable: UITableView!
+    
+    var emojiArray = ["😎","🤷🏻‍♂️","😭","😬","🙏🏻","😈","🙈","🙄","🤓","😡","🐥"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    emojiTable.dataSource = self
+    emojiTable.delegate = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojiArray[indexPath.row]
+        return cell
+    }
 
 }
 
